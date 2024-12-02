d60546dac4d17ffa962019de7f76a5d7d86d295f
stefanmzeidler <stefanmzeidler@gmail.com>
2024-12-01 23:55:38 -0600

    Reverted back to enum because dataclassses are annoying.

--- Zeidler_Homework3.py
+++ Zeidler_Homework3.py
@@ -1,24 +1,33 @@
 import enum
 
-from transitions import Machine, State
+from transitions import Machine, State, Transition
 
 
 class AcademicGoalSetter(Machine):
     def __init__(self):
-        transitions = [[self.Triggers.INITIALIZE.value, self.States.INITIAL_DUMMY.value, self.States.START.value]]
+        with open('token.txt') as token_file:
+            self.token = token_file.readline().strip()
+        transitions = self.load_transitions()
         Machine.__init__(self, states=self.States.values(), transitions=transitions,
                          initial=self.States.INITIAL_DUMMY.value, send_event=True, on_exception = 'to_error',on_final='remove_from_machine')
-        self.initialize()
+        self.run()
+        self.remove_from_machine()
 
-    class Triggers(enum.Enum):
-        INITIALIZE = 'initialize'
+    def load_transitions(self):
+        transitions = [['run', self.States.INITIAL_DUMMY.value, self.States.START.value],
+                       ['help_plan',self.States.IDEA_CHECK.value,self.States.SMART_PLANNING.value],
+                       ['suggest_ideas',self.States.IDEA_CHECK.value,self.States.BEHAVIOR_MENU.value],
+                       ['offer_check_in',self.States.IDEA_CHECK.value,self.States.CHECK_IN_OFFER.value],
+                       ['re_enter', '*', '=']]
+
+        return transitions
 
     class States(enum.Enum):
         ERROR = State(name='error', on_enter = 'handle_error')
         INITIAL_DUMMY = State(name='initial_dummy')
         START = State(name='start', on_enter=['greet', 'to_idea_check'])
-        IDEA_CHECK = State(name='idea_check', on_enter='get_user_action')
-        SMART_PLANNING = State(name='smart_planning')
+        IDEA_CHECK = State(name='idea_check', on_enter='get_user_intention')
+        SMART_PLANNING = State(name='smart_planning', on_enter = 'get_goal_information')
         BEHAVIOR_MENU = State(name='behavior_menu')
         CHECK_IN_OFFER = State(name='check_in_offer')
         COMMITMENT_STATEMENT = State(name='commitment_statement')
@@ -31,48 +40,34 @@ class AcademicGoalSetter(Machine):
         def values(cls):
             return [member.value for member in cls]
 
-    # @dataclass
-    # class States:
-    #     ERROR : State = State(name='error')
-    #     INITIAL_DUMMY : State = State(name='initial_dummy')
-    #     START : State= State(name='start', on_enter= ['greet', 'to_idea_check'])
-    #     IDEA_CHECK : State= State(name='idea_check',on_enter='get_user_action')
-    #     SMART_PLANNING : State= State(name='smart_planning')
-    #     BEHAVIOR_MENU : State= State(name='behavior_menu')
-    #     CHECK_IN_OFFER : State= State(name='check_in_offer')
-    #     COMMITMENT_STATEMENT : State= State(name='commitment_statement')
-    #     SUCCESS_REFLECTION : State= State(name='success_reflection')
-    #     CHECK_ON_PROGRESS : State= State(name='check_on_progress')
-    #     GOODBYE : State= State(name='goodbye')
-    #     FINAL_DUMMY: State = State(name='final_dummy', final = True)
-    #
-    #     def __init__(self, outer):
-    #         self.outer = outer
-    #
-    #     def __iter__(self):
-    #         return iter(list(getattr(self,  field.name) for field in dataclasses.fields(self)))
-
-    def greet(self, event):
+    @staticmethod
+    def greet(event):
         print("Hello!")
 
-    def get_user_action(self, event):
-        response = input("Are there any academic goals you would like to achieve in the next week or two?\n")
-        self.route_user(event, response)
+    def get_user_intention(self, event):
+        print("Are there any academic goals you would like to achieve in the next week or two?")
+        response = input("Please enter yes, no, or not sure:\n")
+        response = response.lower().strip()
+        match response:
+            case 'yes':
+                self.help_plan()
+            case 'no':
+                self.check_in_offer()
+            case 'not sure':
+                self.suggest_ideas()
+            case _:
+                print("I'm sorry, I didn't understand that")
+                self.re_enter()
+    #
+    # def get_goal_information(self, event):
 
-    def route_user(self, event, response):
-        state = event.state
-        print(state)
-        match state:
-            case self.States.IDEA_CHECK.value:
-                print("Got it!")
-                self.to_final_dummy()
-
-    def remove_from_machine(self, event):
-        self.remove_model(self)
-        print("Model removed")
-
-    def get_states(self):
-        return self.states
+    #
+    # def remove_from_machine(self, event=None):
+    #     self.remove_model(self)
+    #     print("Model removed")
+    #
+    # def get_states(self):
+    #     return self.states
 
 
 def main():
---
+++ sentiment_analysis.py
@@ -0,0 +1,78 @@
+import os
+
+import pandas as pd
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from pickle import dump,load
+
+#
+
+# count_vect = CountVectorizer()
+# X_train_counts = count_vect.fit_transform(twenty_train.data)
+text_clf = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), (
+    'clf', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, random_state=42, max_iter=5, tol=None))])
+
+
+#
+# twenty_test = fetch_20newsgroups(subset='test', categories=categories, shuffle=True, random_state=42)
+# text_clf.fit(twenty_train.data, twenty_train.target)
+# docs_test = twenty_test.data
+# predicted = text_clf.predict(docs_test)
+# print(metrics.classification_report(twenty_test.target, predicted,target_names=twenty_test.target_names))
+
+
+class CLFTrainer:
+    def __init__(self, data_file: str = None):
+        self.data = pd.read_csv(data_file, sep=None, engine='python', names=['target', 'features'], header=None,
+                                usecols=[0, 5])
+        self.features = self.data['features']
+        self.target = self.data['target']
+        self.x_test, self.y_test, self.x_training, self.x_validation, self.y_training, self.y_validation = self.__split_data()
+
+    def __split_data(self):
+        """
+        Split the dataset into 80% training and 20% testing sets. The training set is further split into 80% training and 20% validation sets.
+        :return: Returns the x and y test, training, and validation sets.
+        """
+        x_tr, x_test, y_tr, y_test = train_test_split(self.features, self.target, test_size=0.2, random_state=3)
+        x_training, x_validation, y_training, y_validation = train_test_split(x_tr, y_tr, test_size=0.2, random_state=3)
+        return x_test, y_test, x_training, x_validation, y_training, y_validation
+
+    def train_model(self, save = True):
+        model_pipe = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), (
+            'clf', SGDClassifier(loss='hinge'))])
+        parameters = {'vect__ngram_range': [(1, 1), (1, 2),(1,3)], 'tfidf__use_idf': (True, False),
+            'clf__alpha': (1e-2, 1e-3)}
+        text_classifier = GridSearchCV(model_pipe, parameters, cv=10, return_train_score=True)
+        text_classifier.fit(self.features, self.target)
+        best_model = text_classifier.best_estimator_
+        results = pd.DataFrame(text_classifier.cv_results_)
+        if save:
+            with open("sentiment_classifier.pkl", "wb") as f:
+                dump(best_model, f, protocol=5)
+        return best_model
+
+
+def main():
+    cur_dir = os.path.dirname(__file__)
+    classifier_file = os.path.join(cur_dir, 'sentiment_classifier.pkl')
+    if os.path.isfile(classifier_file):
+        with open((os.path.join(cur_dir, 'sentiment_classifier.pkl')), 'rb') as f:
+            classifier = load(f)
+    else:
+        filepath = os.path.join(cur_dir, 'Datasets', 'testdata.manual.2009.06.14.csv')
+        my_trainer = CLFTrainer(filepath)
+        classifier = my_trainer.train_model()
+    print(classifier.get_params())
+
+
+    # categories = ['alt.atheism', 'soc.religion.christian', 'comp.graphics', 'sci.med']
+    # twenty_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
+    # print(twenty_train.target_names)
+
+
+main()
