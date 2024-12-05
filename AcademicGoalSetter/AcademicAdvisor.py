0c9823f555b8c064bb91b1e117efec0a7d711e99
stefanmzeidler <szeidler@uwm.edu>
2024-12-04 21:57:03 -0600

    Branching off the gen ai version.

--- AcademicGoalSetter/AcademicAdvisor.py
+++ AcademicGoalSetter/AcademicAdvisor.py
@@ -2,26 +2,31 @@ import pandas as pd
 from transitions import Machine, State
 import time
 from task_manager import TaskManager
+import os
+import logging
+
 
 
 class AcademicAdvisor(Machine):
-    def __init__(self):
+    def __init__(self, debug=False):
         self.task_manager = TaskManager()
-        self.user_db = pd.read_csv("Users.csv")
-        self.log = []
         Machine.__init__(self, initial='INITIAL_DUMMY', send_event=True,
                          on_final='remove_from_machine')
         self.load_states()
         self.load_transitions()
         self.user_name = None
         self.plan = None
-
+        self.log = []
+        if not os.path.exists("UserData/Users.csv"):
+            self.user_db = pd.read_csv("UserData/Users.csv")
+        else:
+            self.user_db = pd.DataFrame(columns=["User", "Goal", "Commitment"])
 
     def load_states(self):
         self.add_state(State('START'))
         self.add_state(State('IDEA_CHECK'))
         self.add_state(State('SMART_PLANNING'))
-        self.add_state(State('BEHAVIOR_MEN'))
+        self.add_state(State('BEHAVIOR_MENU'))
         self.add_state(State('CHECK_IN_OFFER'))
         self.add_state(State('COMMITMENT_STATEMENT'))
         self.add_state(State('SUCCESS_REFLECTION'))
@@ -42,22 +47,28 @@ class AcademicAdvisor(Machine):
         self.add_transition('get_commitment', ['CONFIDENCE_CHECK', 'SUCCESS_REFLECTION'], 'BEHAVIOR_MENU')
         self.add_transition('restart_advisor', 'GOODBYE', 'START', after = 'begin_session')
         self.add_transition('offer_check_in', ['BEHAVIOR_MENU', 'IDEA_CHECK'], 'CHECK_IN_OFFER')
-
-    def get_plan_details(self):
-        self.plan = self.log_interaction("With your permission, may I ask more details? What, When, Where, How often, and Start Date?")
+        self.add_transition('force_end_session', '*', 'FINAL_DUMMY')
 
     def log_interaction(self, text):
-        self.log.append("Advisor: " + text)
-        response = input(text)
-        self.log.append("Student: " + response)
+        with open(f"UserData/{self.user_name}.txt", 'a') as log:
+            log.write("Advisor: " + text+"\n")
+            response = input(text)
+            if response.lower().strip() == 'exit':
+                self.force_end_session()
+            log.write("Student: " + response+"\n")
         return response
 
-    def log_output(self, text):
-        self.log.append("Advisor: " + text)
+    def log_output(self, text, advisor = True):
+        with open(f"UserData/{self.user_name}.txt", 'a') as log:
+            if advisor:
+                log.write("Advisor: " + text + "\n")
+            else:
+                log.write(text+"\n")
         print(text)
 
     def get_name(self, event):
-        self.user_name = self.log_interaction("Please enter your name: ")
+        self.user_name = input("Please enter your name: ")
+        self.log_output("Session start: " + time.ctime() + "\nType 'exit' to end",advisor=False)
         self.begin_session(first_run=True)
 
     def get_user_intention(self, event):
@@ -77,12 +88,9 @@ class AcademicAdvisor(Machine):
                 self.re_enter()
 
     def remove_from_machine(self, event=None):
-        with open('log.txt', 'a') as file:
-            file.write(time.ctime())
-            for line in self.log:
-                file.write(line)
+        self.log_output("Session end: " + time.ctime(),advisor=False)
         self.remove_model(self)
-        print("Model removed")
+        exit(0)
 
     def get_user_idea_choice(self,event):
         self.log_output("If you're not sure, I can suggest some ideas:")
@@ -90,9 +98,13 @@ class AcademicAdvisor(Machine):
         self.log_output(ideas)
         response = self.log_interaction("Please let me know which one you would like!\n")
         idea_selection = self.task_manager.verify_ideas(ideas,response)
-        print(idea_selection)
+
 
     def handle_error(self, event):
         print("an error has occurred")
         print(event.error)
         self.remove_from_machine(event)
+
+    def get_plan_details(self):
+        self.plan = self.log_interaction(
+            "With your permission, may I ask more details? What, When, Where, How often, and Start Date?")
